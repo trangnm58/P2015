28fa2ee137ff5aea062ca6e303e2ee13c5e7b64e
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2015-07-24 01:01:23 +0700

    update Bucket

--- src/main/Bucket.java
+++ src/main/Bucket.java
@@ -6,19 +6,32 @@ import java.util.HashMap;
 
 public class Bucket {
 	public HashMap<Integer, Piece> pieces = new HashMap<Integer, Piece>();
-	public Piece minSize;
-	private int min = 17; // > max size of a piece
-
+	public int minSize = 17; // > max size of a piece
+	private int minId = 1030;
 	/*
 	 * push piece to pieces
 	 */
 	public boolean push (Piece piece) {
 		pieces.put(piece.id, piece);
-		if (piece.body.length < min) {
-			min = piece.body.length;
-			minSize = piece;
+		if (piece.body.length < minSize) {
+			minSize = piece.body.length;
 		}
+		if (piece.id < minId) minId = piece.id;
 		return true;
+	}
+	
+	/*
+	 * pop the piece with the smallest id
+	 */
+	public Piece pop() {
+		Piece p = get(minId);
+		
+		// update minId
+		Piece next = pieces.get(++minId);
+		while (next == null) {
+			next = pieces.get(++minId);
+		}
+		return p;
 	}
 	
 	/*
