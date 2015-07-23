f35435e1b030a4bf8e1e70b3799d6505e099093e
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2015-07-24 00:28:42 +0700

    add Answer constructor with Board as argument

--- src/main/Answer.java
+++ src/main/Answer.java
@@ -7,6 +7,17 @@ import java.util.HashMap;
 public class Answer {
 	public HashMap<Integer, Line> lines = new HashMap<Integer, Line>();
 
+	public Answer() {}
+	
+	/*
+	 * constructor with Board
+	 */
+	public Answer(Board board) {
+		for (int i=0; i < board.pieceStack.length; i++) {
+			push(board.posStack[i], board.pieceStack[i]);
+		}
+	}
+	
 	public void push (Point ref, Piece p) {
 		Line line = new Line(ref, p);
 		lines.put(p.id, line);
